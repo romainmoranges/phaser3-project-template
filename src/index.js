9ec107ea46ea47ce9ccc8601650c3195cdd1592a
Romain Moranges <pro@romainmoranges.com>
2020-08-04 21:56:19 +0200

    edit linter & use yarn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
 
 const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+	type: Phaser.AUTO,
+	parent: 'phaser-example',
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create
+	}
 };
 
+// eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
+function preload ()
+{
+	this.load.image('logo', logoImg);
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+function create ()
+{
+	const logo = this.add.image(400, 150, 'logo');
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+	this.tweens.add({
+		targets: logo,
+		y: 450,
+		duration: 2000,
+		ease: 'Power2',
+		yoyo: true,
+		loop: -1
+	});
 }
